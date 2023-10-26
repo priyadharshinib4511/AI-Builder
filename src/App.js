01a6fca7cd960b303349fa17d2ff2d60e68bff87
Anjali Kumari <Anjali.Kumari.Contractor@pepsico.com>
2023-10-26 19:39:27 +0530

    route added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
-import './App.css';
-import Sidebar from './components/Topbar';
-import Boxes from './components/boxes/Boxes';
-import Tools from './components/boxes/Tools';
-import TableComp from './components/table/TableComp';
-import Example from './pages/SNOWAnalyser';
+import "./App.css";
+import Sidebar from "./components/Topbar";
+import Example from "./pages/SNOWAnalyser";
 
 function App() {
   return (
     <>
-    <Example />
-    <Tools />
-    <Boxes/> 
-    <TableComp />
-  </>
+      <Example />
+    </>
   );
 }
 
